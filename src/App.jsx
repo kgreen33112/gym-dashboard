f4d80c549277d3984aca395732adef9ee538c80d
kgreen33112 <kgreencoprods@gmail.com>
2026-05-24 21:19:15 -0500

    Update packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-
+import { Routes, Route, Navigate } from "react-router-dom";
 import './App.css'
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
